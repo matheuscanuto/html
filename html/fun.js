8707391657fe1bd51de3caab4429c0e660d663fa
matheuscanuto <83411828+matheuscanuto@users.noreply.github.com>
2021-05-01 11:03:22 -0300

    html files

---
+++ html/fun.js
@@ -0,0 +1,10 @@
+function insert(){
+  	var name = document.getElementById('name').value;
+  	var number = document.getElementById('number').value;
+
+    var h = document.getElementById('m');
+    var i = document.getElementById('n');
+    h.innerText = name;
+    i.innerText = number;
+
+}
